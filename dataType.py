95bdb0c6308b043615550559025eac63f6fc3527
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-18 20:37:45 +0100

    Data Types in Python

---
+++ dataType.py
@@ -0,0 +1,26 @@
+x = 5;
+print(type(x));
+
+t = 56.0;
+print(type(t));
+
+y = 'hi!';
+print(type(y));
+
+m = list(("orange", "banana", "mango"));
+print(type(m));
+
+n = tuple(("orange", "carrot", "apple"));
+print(type(n));
+
+o = True;
+print(type(o));
+
+r = range(6);
+print(type(r));
+
+z = ["name", "Esther"];
+print(type(z));
+
+s = set(("colour", "range"));
+print(type(s));
