181e5501cced19494b54f171a0c55776be90c63f
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-18 17:26:10 +0100

    First Python Program

---
+++ index.py
@@ -0,0 +1,5 @@
+print("It's a new day");
+print("Everything is gonna be alright");
+print("I'm gonna learn how to automate my test case");
+print("I'm gonna become an automated tester");
+print("I'm gonna get a better paying gig this month, before this week ends");
