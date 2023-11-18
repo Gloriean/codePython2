288dcedd5ff85b4e816a97c6780e07c11f25d79b
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-18 18:17:05 +0100

    First Program in Python

--- index.py
+++ index.py
@@ -3,3 +3,34 @@ print("Everything is gonna be alright");
 print("I'm gonna learn how to automate my test case");
 print("I'm gonna become an automated tester");
 print("I'm gonna get a better paying gig this month, before this week ends");
+
+#declaring and assigning value to a variable
+num = 1
+letter = "network"
+
+
+print(num);
+print(letter);
+
+print(type(num));
+print(type(letter));
+
+x, y, z = "orange", "watermelon", "pineapple";
+print(x);
+print(y);
+print(z);
+
+food = ["rice", "beans", "garri"];
+a,b,c = food;
+print(a);
+print(b);
+print(c);
+
+m = "awesome";
+
+def myFunc():
+    m = 'fantastic';
+    print("Python is", m);
+myFunc()
+
+print("Python is", m);
