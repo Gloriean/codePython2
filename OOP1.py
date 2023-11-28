4458cf1b09523048999b677f8bd9db3aa22af614
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-11-28 17:54:38 +0100

    Object Oriented Programming

--- OOP1.py
+++ OOP1.py
@@ -10,6 +10,7 @@ p = HND("Glory", "Company")
 p.show()
 
 
+#Inheritance
 #Create a class named Person, with firstname and lastname properties, and a printname method:
 class Person:
     def __init__(self, fname, lname):
@@ -21,3 +22,56 @@ class Person:
 
 d = Person("Ope", "Ayorinde")
 d.printname()
+
+
+#Iteration
+fruits = ["apple", "banana", "carrot"]
+
+myit = iter(fruits)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+#Iterating a String
+str = 'Iterating'
+
+myit = iter(str)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+#Looping through an iteration
+
+colour = ["red", "blue", "yellow", "purple"]
+
+for x in colour:
+    print(x)
+
+
+#Create an iterator that returns numbers, starting with 1, and each sequence will increase by one (returning 1,2,3,4,5 etc.):
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
