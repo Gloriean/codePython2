3b8d1d505922c0f687ff664c2925d5558bc345a5
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-12-02 21:59:12 +0100

    Import Module in Python

--- calc.py
+++ calc.py
@@ -4,9 +4,9 @@ print(mt.sqrt(25))
 
 print(mt.pi)
 
-from datetime import date
+from datetime import datetime
 
-today = date.today()
+today = datetime.today()
 
 print("Current Year is:", today.year)
 print("Current Month is:", today.month)
@@ -28,6 +28,18 @@ from datetime import time
 
 Time = time(12, 22, 45)
 
-print("Hour:", Time.hour)
+print("\nHour:", Time.hour)
 print("Minute:", Time.minute)
 print("Seconds:", Time.second)
+
+
+from datetime import datetime
+
+a = datetime(1999, 12, 12, 12, 12, 12, 12)
+print("\nYear =", a.year)
+print("Month =", a.month)
+print("Day =", a.day)
+print("Hour =", a.hour)
+print("Minute =", a.minute)
+print("Seconds =", a.second)
+print("Timestamp =", a.timestamp())
