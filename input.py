2e39f402d5a29cb26e8fe5edadaa8d62dfc40edb
Glory Olasanmi <gloryolasanmi@gmail.com>
2024-01-04 17:19:01 +0100

    How to Take Input From Users

---
+++ input.py
@@ -0,0 +1,20 @@
+#Input Values
+#num1 = input("Enter First Number: ")
+#num2 = input("Enter Second Number: ")
+
+#print(type(num1))
+#print(type(num2))
+
+
+#sum = num1 + num2
+#print(sum)
+#print(type(sum))
+
+#Conversion of an Inputted Value
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+Sum = num1 + num2
+print("Sum is: ", Sum)
+print(type(Sum))
+
